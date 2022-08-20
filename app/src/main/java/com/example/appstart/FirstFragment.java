2ae93e26d647f79e895b2a1803086989478aebe3
Abdalaziz Alsaydi <>
2022-08-20 16:39:31 -0400

    added sensors list

--- app/src/main/java/com/example/appstart/FirstFragment.java
+++ app/src/main/java/com/example/appstart/FirstFragment.java
@@ -1,9 +1,13 @@
 package com.example.appstart;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +15,14 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.appstart.databinding.FragmentFirstBinding;
 
-public class FirstFragment extends Fragment {
+import java.util.List;
 
+public class FirstFragment extends Fragment {
     private FragmentFirstBinding binding;
+    private SensorManager sensorManager;
+    private ListView sensorListView;
+    private ListAdapter listAdapter;
+    private List<Sensor> sensors;
 
     @Override
     public View onCreateView(
@@ -22,8 +31,18 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        buildSensorsList();
         return binding.getRoot();
 
+    }
+
+    private void buildSensorsList() {
+        sensorManager = (SensorManager) this.getActivity().getSystemService(Context.SENSOR_SERVICE);
+        if(sensorManager != null) {
+            sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+            listAdapter = new ListAdapter(sensors, this.getActivity());
+            binding.sensorsView.setAdapter(listAdapter);
+        }
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/appstart/ListAdapter.java
@@ -0,0 +1,71 @@
+package com.example.appstart;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.List;
+
+public class ListAdapter extends BaseAdapter implements android.widget.ListAdapter {
+
+    private final List<Sensor> sensors;
+    private final Activity activity;
+
+    public ListAdapter(List<Sensor> sensors, Activity activity) {
+        this.sensors = sensors;
+        this.activity = activity;
+    }
+    @Override
+    public boolean areAllItemsEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(int i) {
+        return i >= 0 && i < sensors.size();
+    }
+
+    @Override
+    public int getCount() {
+        return sensors.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        if(sensors == null || i < 0 || i >= sensors.size()) {
+            return null;
+        }
+        return sensors.get(i);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public long getItemId(int i) {
+        Sensor sensor = (Sensor) getItem(i);
+        if (sensor == null) {
+            return -1L;
+        }
+        return sensor.getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null) {
+            convertView = activity.getLayoutInflater().inflate(R.layout.sensors_list, parent, false);
+        }
+        TextView sensorName = convertView.findViewById(R.id.sensor_name);
+        sensorName.setText(((Sensor)getItem(position)).getName());
+        return convertView;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return sensors == null || sensors.isEmpty();
+    }
+}
